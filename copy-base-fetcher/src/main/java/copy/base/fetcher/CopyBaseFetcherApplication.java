c3e0e605b82b7a341102a96f5c7539821ebc42c5
Karol Mendza <karolmendza@gmail.com>
2019-12-06 22:57:39 +0100

    Initial commit for data fetcher

---
+++ copy-base-fetcher/src/main/java/copy/base/fetcher/CopyBaseFetcherApplication.java
@@ -0,0 +1,13 @@
+package copy.base.fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyBaseFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopyBaseFetcherApplication.class, args);
+    }
+
+}
---
+++ copy-base-fetcher/src/test/java/copy/base/fetcher/CopyBaseFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package copy.base.fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyBaseFetcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
